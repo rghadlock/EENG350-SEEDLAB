55383908cde28f23b5f8c0b856667df54818c174
NolanEgging <79224992+NolanEgging@users.noreply.github.com>
2021-03-13 18:01:38 -0700

    Update RotationControl2.ino

--- Demo1/RotationControl2.ino
+++ Demo1/RotationControl2.ino
@@ -141,7 +141,7 @@ void loop() {
 
     // determines desired angular speeds
     desAngSpeed_R = Kv_R * errorAng_R;
-    desAngSpeed_L = Kv_L * errorAng_R;
+    desAngSpeed_L = Kv_L * errorAng_L;
     if (desAngSpeed_R > ANG_SATURATION) desAngSpeed_R = ANG_SATURATION;
     else if (desAngSpeed_R < -ANG_SATURATION) desAngSpeed_R = -ANG_SATURATION;
     if (desAngSpeed_L > ANG_SATURATION) desAngSpeed_L = ANG_SATURATION;
